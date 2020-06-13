cfe545369eb92320454a1dd5c6fd8c08effd28b6
ecarracedo <65194511+ecarracedo@users.noreply.github.com>
2020-06-13 16:37:06 -0300

    Update contacto.js

--- contacto.js
+++ contacto.js
@@ -1,29 +1,35 @@
 $(document).ready(function() {
+    // Toma los datos de un form con id='loginform' cuando hace submit
     $('#loginform').submit(function(e) {
         e.preventDefault();
         $.ajax({
-
+            // envia los datos por POST
             type: "POST",
-            url: "vendor/captcha/contacto.php",
+            url: "contacto.php",
             data: $(this).serialize(),
             success: function(response)
-
             {
-              var jsonData = JSON.parse(response)
-              
-              if (jsonData.clave == 0){
+              // Almacena la respuesta JSON de contacto.php en una variable
+              // JSON esta formado por $json('html' => 'el html de respuesta', 'clave' => 0 o 1 dependiendo si pasa o no el captcha )
+                var jsonData = JSON.parse(response)
+                
+                if (jsonData.clave == 0){
+                    //Aca seguramente dio mal el captcha, por lo que la clave es = 0 y la respuesta en html es negativa
+                    //Devuelve el html a un div con id='messages'
+                    $('#messages').html(jsonData.html);
 
-                $('#messages').html(jsonData.html);
+                }else{
 
-              }else{
-
-                $('#messages').html(jsonData.html);
-                $("#loginform")[0].reset();
-                grecaptcha.reset();
-
-              }
-             
+                    //Aca seguramente dio bien el captcha, por lo que la clave es = 1 y la respuesta en html es positiva
+                    //Devuelve el html a un div con id='messages'
+                    $('#messages').html(jsonData.html);
+                    //Borra los datos del form
+                    $("#loginform")[0].reset();
+                    //Resetea el captcha
+                    grecaptcha.reset();
+                }
+            
             }
-      });
+        });
     });
 });
